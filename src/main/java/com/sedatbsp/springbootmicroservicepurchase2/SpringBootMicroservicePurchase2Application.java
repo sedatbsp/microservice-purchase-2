723f927787f56be28edff51567843484d5ce36c6
Sedat Başpınar <sdtbsp10@gmail.com>
2022-04-24 05:26:49 +0300

    init commit

---
+++ src/main/java/com/sedatbsp/springbootmicroservicepurchase2/SpringBootMicroservicePurchase2Application.java
@@ -0,0 +1,13 @@
+package com.sedatbsp.springbootmicroservicepurchase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicePurchase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicePurchase2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sedatbsp/springbootmicroservicepurchase2/SpringBootMicroservicePurchase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedatbsp.springbootmicroservicepurchase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicePurchase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
